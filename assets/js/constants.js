12a79dc9f8872cb059e042432818746e6df52e16
José dBruxelles <18559798+jdbruxelles@users.noreply.github.com>
2022-05-14 13:10:29 +0200

    Update the app with new features
    - Can now render SVG files
    - Preview type unified
    - Optimisation
    - Remove duplicated loading of gist
    - Improve UI style for Markdown rendering
    - Make all render with easy and beautifully!

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -3,11 +3,11 @@ let hljs_all_themes = {
     "Atom One Dark", "Agate", "An Old Hope", "Androidstudio", "Dracula",
     "Github Dark", "Github Dark Dimmed", "Gradient Dark", "Marrakesh",
     "Monokai Sublime", "Railscasts", "Rainbow", "Solarized Dark",
-    "Tomorrow Night Blue"
+    "Tomorrow Night Blue", "Xt256", "Zenburn"
   ],
   light: [
     "Atom One Light", "Color Brewer", "Default", "Github", "Gradient Light",
-    "Mono Blue", "Solarized Light", "Tomorrow", "VS", "Xt256", "Zenburn"
+    "Mono Blue", "Solarized Light", "Tomorrow", "VS"
   ],
   base16: [ // Dark or light themes in the base16 directory.
     "dracula", "marrakesh", "railscasts", "solarized-dark", "solarized-light",
